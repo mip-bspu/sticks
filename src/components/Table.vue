ce3016e69db33224bccf650c6239215a12fdbe51
pyureshka <polinapyura@mail.ru>
2022-08-26 17:06:27 +0900

    change: table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="card">
     <h4>
-      <span>Навес</span>
-      <button>+ Добавить</button>
+      <span></span>
+      <button>+ деталь</button>
     </h4>
 
     <hr />
@@ -10,30 +10,22 @@
       <table>
         <thead>
           <tr>
-            <th>Имя</th>
-            <th>Имя</th>
-            <th>Имя</th>
-            <th>Имя</th>
+            <th>Название</th>
+            <th>Высота</th>
+            <th>Длина</th>
+            <th>Ширина</th>
+            <th>Кол-во</th>
+            <th>Возможность увеличить</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
+            <td><input type="text" name="name" v-model="eName" /></td>
+            <td><input type="number" name="height" v-model="eHeight" /></td>
+            <td><input type="number" name="length" v-model="eLength" /></td>
+            <td><input type="number" name="width" v-model="eWidgth" /></td>
+            <td><input type="number" name="count" v-model="eCount" /></td>
+            <td><input type="checkbox" name="check" v-model="eInc" /></td>
           </tr>
         </tbody>
       </table>
@@ -41,8 +33,18 @@
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { getItem } from "./Item";
+
+let { item, arrayItems } = $(getItem());
+let eName = $ref(null);
+let eHeight = $ref(null);
+let eLength = $ref(null);
+let eWidgth = $ref(null);
+let eCount = $ref(null);
+let eInc = $ref(null);
+
+function setnNewComponent() {}
 </script>
 
 <style>
