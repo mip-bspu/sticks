6b483d346299b9e36a2b75f8e9c09d2a0ad5dd69
pyureshka <polinapyura@mail.ru>
2022-09-05 18:38:32 +0900

    add: fun check field (test)

--- src/composables/useItems.js
+++ src/composables/useItems.js
@@ -1,173 +1,223 @@
 import { computed } from "vue"
 
+let sticks = $ref([
+  {
+    height: 6000,
+    length: 30,
+    width: 30
+  }
+])
+
 let arrayItems = $ref([
+  {
+    name: "Навес",
+    saw: 1,
+    components: [{
+      name: "Вертикальные стойки",
+      height: 8000,
+      length: 30,
+      width: 30,
+      heightSelf: 2000,
+      count: 8,
+    },
     {
-        name: "Навес",
-        saw: 1,
-        components: [{
-            name: "Вертикальные стойки",
-            height: 8000,
-            length: 30,
-            width: 30,
-            heightSelf: 2000,
-            count: 8,
-        },
-        {
-            name: "Горизонтальные стойки (4,5м)",
-            height: 6000,
-            length: 30,
-            width: 30,
-            heightSelf: 4500,
-            count: 2,
-        },
-        {
-            name: "Горизонтальные стойки (6м)",
-            height: 8000,
-            length: 30,
-            width: 30,
-            heightSelf: 6000,
-            count: 2,
-        },
-        {
-          name: "Декор",
-          height: 6000,
-          length: 30,
-          width: 60,
-          heightSelf: 1380,
-          count: 6,
-        },
-      ]
-    }
+      name: "Горизонтальные стойки (4,5м)",
+      height: 6000,
+      length: 30,
+      width: 30,
+      heightSelf: 4500,
+      count: 2,
+    },
+    {
+      name: "Горизонтальные стойки (6м)",
+      height: 8000,
+      length: 30,
+      width: 30,
+      heightSelf: 6000,
+      count: 2,
+    },
+    {
+      name: "Декор",
+      height: 6000,
+      length: 30,
+      width: 60,
+      heightSelf: 1380,
+      count: 6,
+    },
+    ]
+  }
 ])
 
 let chosenItem = $ref(arrayItems[0])
 
 function calcArea(item) {
-    let s = $ref(0)
+  let s = $ref(0)
 
-    if (item) {
-        let comp = item.components
-        for (var i in comp) {
-            s += 2 * comp[i].heightSelf * (comp[i].length + comp[i].width) * comp[i].count
-        }
+  if (item) {
+    let comp = item.components
+    for (var i in comp) {
+      s += 2 * comp[i].heightSelf * (comp[i].length + comp[i].width) * comp[i].count
     }
-    return s / 1000
+  }
+  return s / 1000
 }
 
 function breakIntoGroups(item) {
-    let comp = item.components
-    let groups = []
-    let group = null
-    let index = null
+  let comp = item.components
+  let groups = []
+  let group = null
+  let index = null
 
-    for (var i in comp) {
-        group = groups.find((g) => {
-            if (g.name === (comp[i].length + "x" + comp[i].width + "x" + comp[i].height)) {
-                return g
-            } else {
-                return null
-            }
-        })
+  for (var i in comp) {
+    group = groups.find((g) => {
+      if (g.name === (comp[i].length + "x" + comp[i].width + "x" + comp[i].height)) {
+        return g
+      } else {
+        return null
+      }
+    })
 
-        if (group) {
-            index = groups.indexOf(group)
-            groups[index].components.push({
-                name: comp[i].name,
-                height: comp[i].height,
-                length: comp[i].length,
-                width: comp[i].width,
-                heightSelf: comp[i].heightSelf + item.saw,
-                count: comp[i].count
-            })
-            group = null
-        } else {
-            groups.push({
-                name: comp[i].length + "x" + comp[i].width + "x" + comp[i].height,
-                components: [{
-                    name: comp[i].name,
-                    height: comp[i].height,
-                    length: comp[i].length,
-                    width: comp[i].width,
-                    heightSelf: comp[i].heightSelf + item.saw,
-                    count: comp[i].count
-                }]
-            })
-        }
+    if (group) {
+      index = groups.indexOf(group)
+      groups[index].components.push({
+        name: comp[i].name,
+        height: comp[i].height,
+        length: comp[i].length,
+        width: comp[i].width,
+        heightSelf: comp[i].heightSelf + item.saw,
+        count: comp[i].count
+      })
+      group = null
+    } else {
+      groups.push({
+        name: comp[i].length + "x" + comp[i].width + "x" + comp[i].height,
+        components: [{
+          name: comp[i].name,
+          height: comp[i].height,
+          length: comp[i].length,
+          width: comp[i].width,
+          heightSelf: comp[i].heightSelf + item.saw,
+          count: comp[i].count
+        }]
+      })
     }
+  }
 
-    return groups
+  return groups
 }
 
 function binPacking(item) {
-    let sumHeight = 0
-    let f = false
-    let bin = []
-    let check = null
+  let sumHeight = 0
+  let f = false
+  let bin = []
+  let check = null
 
-    let detail = null
-    let index = null
+  let detail = null
+  let index = null
 
-    let groups = breakIntoGroups(item)
+  let groups = breakIntoGroups(item)
 
-    for (var i in groups) {
-        bin.push({ name: groups[i].name, stick: [{ details: [], tail: groups[i].components[0].height }] })
-        for (var j in groups[i].components) {
-            for (let l = 0; l < groups[i].components[j].count; l++) {
-                for (var k in bin[i].stick) {
-                    check = bin[i].stick[k].tail - groups[i].components[j].heightSelf
-                    if (check >= 0 || -check <= item.saw) {
-                        bin[i].stick[k].tail - groups[i].components[j].heightSelf - item.saw <= 0 ? groups[i].components[j].heightSelf -= item.saw : groups[i].components[j].heightSelf
+  for (var i in groups) {
+    bin.push({ name: groups[i].name, stick: [{ details: [], tail: groups[i].components[0].height }] })
+    for (var j in groups[i].components) {
+      for (let l = 0; l < groups[i].components[j].count; l++) {
+        for (var k in bin[i].stick) {
+          check = bin[i].stick[k].tail - groups[i].components[j].heightSelf
+          if (check >= 0 || -check <= item.saw) {
+            bin[i].stick[k].tail - groups[i].components[j].heightSelf - item.saw <= 0 ? groups[i].components[j].heightSelf -= item.saw : groups[i].components[j].heightSelf
 
-                        detail = bin[i].stick[k].details.find((d) => { if (d.name === groups[i].components[j].name) { return d } else { return null } })
-                        if (detail) {
-                            index = bin[i].stick[k].details.indexOf(detail)
-                            bin[i].stick[k].details[index].countInStick++
-                        } else {
-                            bin[i].stick[k].details.push({
-                                ...groups[i].components[j],
-                                countInStick: 1
-                            })
-                        }
-
-                        detail = null
-
-                        // bin[i].stick[k].details.push(groups[i].components[j])
-                        sumHeight = bin[i].stick[k].details.reduce(function (sum, elem) {
-                            return sum + elem.heightSelf * elem.countInStick;
-                        }, 0);
-                        bin[i].stick[k].tail = groups[i].components[0].height - sumHeight
-                        f = true
-                        bin[i].stick.sort((a, b) => a.tail - b.tail)
-                        break
-                    }
-                }
-                if (!f) {
-                    bin[i].stick.push({
-                        details: [{
-                            ...groups[i].components[j],
-                            countInStick: 1
-                        }],
-                        tail: groups[i].components[0].height - groups[i].components[j].heightSelf
-                    })
-                    bin[i].stick.sort((a, b) => a.tail - b.tail)
-                }
-                f = false
+            detail = bin[i].stick[k].details.find((d) => { if (d.name === groups[i].components[j].name) { return d } else { return null } })
+            if (detail) {
+              index = bin[i].stick[k].details.indexOf(detail)
+              bin[i].stick[k].details[index].countInStick++
+            } else {
+              bin[i].stick[k].details.push({
+                ...groups[i].components[j],
+                countInStick: 1
+              })
             }
-        }
-    }
 
-    console.log(bin);
-    return bin
+            detail = null
+
+            sumHeight = bin[i].stick[k].details.reduce(function (sum, elem) {
+              return sum + elem.heightSelf * elem.countInStick;
+            }, 0);
+            bin[i].stick[k].tail = groups[i].components[0].height - sumHeight
+            f = true
+            bin[i].stick.sort((a, b) => a.tail - b.tail)
+            break
+          }
+        }
+        if (!f) {
+          bin[i].stick.push({
+            details: [{
+              ...groups[i].components[j],
+              countInStick: 1
+            }],
+            tail: groups[i].components[0].height - groups[i].components[j].heightSelf
+          })
+          bin[i].stick.sort((a, b) => a.tail - b.tail)
+        }
+        f = false
+      }
+    }
+  }
+
+  console.log(bin);
+  return bin
+}
+
+//!
+function checkFields(components) {
+  let msg = null
+  let check = {}
+  debugger
+
+  for (var i in components) {
+    for (var k in components[i]) {
+      if (isNaN(components[i][k])) {
+        msg = checkName(components[i][k])
+        if (msg) {
+          check[i] = { k: msg }
+        }
+      } else {
+
+      }
+    }
+  }
+}
+
+function checkNumber(num, height) {
+  switch (num) {
+    case num < 0: {
+      return ""
+    };
+    case num < height: {
+      return ""
+    }
+    case num === null: {
+      return ""
+    }
+  }
+}
+
+function checkName(name) {
+  debugger
+  if (chosenItem.components.find((c) => c.name === name)) {
+    return "Деталь с таким названием уже существует"
+  }
 }
 
 let sPaint = computed(() => calcArea(chosenItem))
 let countSticks = computed(() => binPacking(chosenItem))
+let errors = computed(() => checkFields(chosenItem.components));
 
 export function useItems() {
-    return $$({
-        sPaint,
-        arrayItems,
-        chosenItem,
-        countSticks
-    })
+  return $$({
+    sPaint,
+    arrayItems,
+    chosenItem,
+    countSticks,
+    errors
+  })
 }
