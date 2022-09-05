59346c44017bc702eff4a61de312ea77ca9fbb8c
pyureshka <polinapyura@mail.ru>
2022-09-05 18:36:46 +0900

    add: navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TableC from "./components/Table.vue";
 import ListItems from "./components/ListItems.vue";
+import Navbar from "./components/Nav.vue";
 </script>
 
 <template>
+  <navbar />
   <list-items />
   <table-c />
 </template>
---
+++ src/components/Nav.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="nav">
+    <li>Изделия</li>
+    <li>Хлысты</li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin-bottom: 1rem;
+  padding: 0;
+  overflow: hidden;
+  background-color: #0077e6;
+  color: aliceblue;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+li {
+  float: left;
+}
+
+li:hover {
+  color: #0077e6;
+  background-color: aliceblue;
+}
+</style>
