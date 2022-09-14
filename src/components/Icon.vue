7bb0109523e595f5acd43d6987b32d99439c2724
pyureshka <polinapyura@mail.ru>
2022-09-14 09:29:02 +0900

    add: icons pack

---
+++ src/components/Icon.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  path: String,
+});
+</script>
+
+<template>
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path :d="path" />
+  </svg>
+</template>
+
+<style scoped>
+svg {
+  width: 1.5rem;
+}
+</style>
