39ec2c16902f80abf8fbbc2729ea7eff957eee6f
pyureshka <polinapyura@mail.ru>
2022-09-15 15:53:27 +0900

    fix: store

--- src/composables/useStore.js
+++ src/composables/useStore.js
@@ -1,59 +1,11 @@
 import { watch } from "vue"
+import { useId } from "./useId"
+let { getId } = $(useId())
 
 let key = "sticks";
 
-let store = $ref({
-  arrayItems: [],
-  sticks: []
-})
-
-// test:
-store.sticks = [
-  {
-    id: 0,
-    height: 6000,
-    length: 30,
-    width: 30
-  },
-  {
-    id: 1,
-    height: 6000,
-    length: 30,
-    width: 60
-  },
-]
-
-store.arrayItems = [
-  {
-    name: "Навес",
-    saw: 1,
-    components: [{
-      name: "det1",
-      stickId: 0,
-      heightSelf: 2000,
-      count: 2,
-    },
-    {
-      name: "det2",
-      stickId: 0,
-      heightSelf: 1500,
-      count: 2,
-    },
-    {
-      name: "det3",
-      stickId: 0,
-      heightSelf: 6000,
-      count: 2,
-    },
-    {
-      name: "det4",
-      stickId: 1,
-      heightSelf: 2000,
-      count: 3,
-    }
-    ]
-  }
-]
+let defaultV = $ref({ arrayItems: [], sticks: [] })
+let store = $ref(JSON.parse(localStorage.getItem(key) || JSON.stringify(defaultV)))
 
 watch(() => store, save, { deep: true, immediate: true })
 
@@ -62,8 +14,7 @@ function save(store) {
 }
 
 function read() {
-  let v = localStorage.getItem(key) || "{}"
-  Object.assign(store, JSON.parse(v))
+  Object.assign(store, JSON.parse(localStorage.getItem(key) || store))
 }
 
 read()
